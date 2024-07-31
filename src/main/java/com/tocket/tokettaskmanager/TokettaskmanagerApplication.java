d469b29eb3fe9630d840561caa2f86de4e07cc34
Parth Dundh <french-fri@users.noreply.github.com>
2024-07-31 23:38:55 +0530

    Initial Project Setup

---
+++ src/main/java/com/tocket/tokettaskmanager/TokettaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.tocket.tokettaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokettaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokettaskmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tocket/tokettaskmanager/TokettaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tocket.tokettaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokettaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
